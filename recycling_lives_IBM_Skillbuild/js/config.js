c221578ae07afbb6f5da3bfbad1775de1b3652a3
Rudra Jain <100768478+R-J003@users.noreply.github.com>
2024-08-01 12:34:14 +0530

    Add files via upload

---
+++ recycling_lives_IBM_Skillbuild/js/config.js
@@ -0,0 +1,14 @@
+// Firebase Configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCARrjU65e7tI9D46k2FI5NDHF_YlpTXm4",
+    authDomain: "fir-ibm-project.firebaseapp.com",
+    databaseURL: "https://fir-ibm-project-default-rtdb.firebaseio.com/",
+    projectId: "fir-ibm-project",
+    storageBucket: "fir-ibm-project.appspot.com",
+    messagingSenderId: "132738009955",
+    appId: "1:132738009955:web:2472ce28af0e837b8be156",
+    measurementId: "G-JVWBLNC8VQ"
+  };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
